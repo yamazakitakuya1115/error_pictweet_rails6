291d25155f62ccbefc5773e1d9878e89aff80acd
yamazakitakuya <71809256+yamazakitakuya1115@users.noreply.github.com>
2020-12-05 16:02:36 +0900

    first_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets, only: [:new, :index, :edit, :show, :update, :destroy] do  
+  resources :tweets do  
     resources :comments, only: :create
     collection do
       get 'search'
